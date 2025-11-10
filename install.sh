cef6f431ce69ab77d22b0fe3bc93365bbab705a3
André Dreyer <1818388+dr3dr3@users.noreply.github.com>
2025-11-10 17:11:54 +1000

    Refactor install script for dotfiles installation
    Updated the script to copy dotfiles from the current directory and removed the git configuration lines.

--- install.sh
+++ install.sh
@@ -2,14 +2,8 @@
 
 # Devpod runs this script when using dotfiles
 
-# Set git user and email 
-git config --global user.name "Andre Dreyer"
-git config --global user.email "git@andredreyer.com"
-
 # Install dotfiles
-cp -r /workspace/.dotfiles ~/dotfiles/
-mv ~/dotfiles/.dotfiles ~
-rm -r ~/dotfiles
+cp -r ./.dotfiles ~/.dotfiles/
 cd ~/.dotfiles
 stow */
 
